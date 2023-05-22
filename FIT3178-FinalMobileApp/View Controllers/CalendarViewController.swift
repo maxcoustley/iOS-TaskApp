fe6db5c1fe13deabd356515d236ea7236c353a38
Max Coustley <mcou0008@student.monash.edu>
2023-05-22 13:26:04 +1000

    basic calendar screen implemented using FSCalendar

---
+++ FIT3178-FinalMobileApp/View Controllers/CalendarViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CalendarViewController.swift
+//  FIT3178-FinalMobileApp
+//
+//  Created by Max Coustley on 22/5/2023.
+//
+
+import UIKit
+import FSCalendar
+
+class CalendarViewController: UIViewController, FSCalendarDelegate {
+
+    @IBOutlet weak var calendarView: FSCalendar!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
